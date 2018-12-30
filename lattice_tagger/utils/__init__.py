dd47eaa54d2a5aeb5fd1eafc505dc63d7777a39c
lovit <soy.lovit@gmail.com>
2018-12-31 05:18:47 +0900

    Commit sentence lookup func & LRLookup

--- lattice_tagger/utils/__init__.py
+++ lattice_tagger/utils/__init__.py
@@ -1,2 +1,4 @@
 from .dictionary import WordDictionary
 from .dictionary import MorphemeDictionary
+from .lookup import sentence_lookup
+from .lookup import LRLookup
---
+++ lattice_tagger/utils/lookup.py
@@ -0,0 +1,97 @@
+from lattice_tagger.tagset import *
+
+def sentence_lookup(sent, eojeol_lookup):
+    """
+    Arguments
+    ---------
+    sent : str
+        String type sentence
+    eojeol_lookup : Lookup function
+
+    Returns
+    -------
+    nodes : list of Word
+
+    Usage
+    ----
+        >>> dictionary = MorphemeDictionary(tag_to_morphs)
+        >>> eojeol_lookup = LRLookup(dictionary)
+
+        >>> sent = '너무너무너무는 아이오아이의 노래입니다'
+        >>> sentence_lookup(sent, eojeol_lookup)
+
+        $ [(Word(너무너무너무, 너무너무너무/Noun, len=6), 0, 6),
+           (Word(는, 는/Josa, len=1), 6, 7),
+           (Word(아이오아이, 아이오아이/Noun, len=5), 7, 12),
+           (Word(의, 의/Josa, len=1), 12, 13),
+           (Word(노래, 노래/Noun, len=2), 13, 15),
+           (Word(입니다, 이/Adjective + ㅂ니다/Eomi, len=3), 15, 18)]
+    """
+
+    offset = 0
+    nodes = []
+    for eojeol in sent.split():
+        nodes += eojeol_lookup(eojeol, offset)
+        offset += len(eojeol)
+    return nodes
+
+class LRLookup:
+    """
+    Assume that eojeol has no space noise
+
+    Usage
+    -----
+        >>> morph_dict = MorphemeDictionary(tag_to_morphs)
+        >>> morph_dict.as_Word('했다')
+        $ [Word(했다, 하/Verb + 았다/Eomi, len=2)]
+
+        >>> morph_dict.as_Word('공연')
+        $ [Word(공연, 공연/Noun, len=2)]
+
+        >>> eojeol_lookup = LRLookup(morph_dict)
+        >>> eojeol_lookup('공연했다')
+        $ [(Word(공연, 공연/Noun, len=2), 0, 2), (Word(했다, 하/Verb + 았다/Eomi, len=2), 2, 4)]
+
+        >>> eojeol_lookup('공연했다', offset=3)
+        $ [(Word(공연, 공연/Noun, len=2), 3, 5), (Word(했다, 하/Verb + 았다/Eomi, len=2), 5, 7)]
+
+    """
+
+    def __init__(self, dictionary, templates=None):
+        self.dictionary = dictionary
+        if templates is None:
+            templates = self._default_templates()
+        self.templates = templates
+
+    def _default_templates(self):
+        templates = [
+            (Noun, Josa),
+            (Noun, Adjective),
+            (Noun, Verb),
+        ]
+        return templates
+
+    def __call__(self, eojeol, offset=0):
+        return self.lookup(eojeol, offset)
+
+    def lookup(self, eojeol, offset=0):
+        def accept(l_, r_, template):
+            return l_.tag0 == template[0] and r_.tag0 == template[1]
+
+        words = self.dictionary.as_Word(eojeol)
+
+        if words:
+            return [(word, offset, offset + word.len) for word in words]
+
+        n = len(eojeol)
+        for i in range(1, n):
+            l = self.dictionary.as_Word(eojeol[:i])
+            r = self.dictionary.as_Word(eojeol[i:])
+            for l_ in l:
+                for r_ in r:
+                    for template in self.templates:
+                        if not accept(l_, r_, template):
+                            continue
+                        words.append((l_, offset, offset + l_.len))
+                        words.append((r_, offset + l_.len, offset + l_.len + r_.len))
+        return words
