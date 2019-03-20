9ab6f0e339d46f7a3d287d041e93b872a89ea3dc
lovit <soy.lovit@gmail.com>
2019-03-21 03:56:47 +0900

    Separe subword_lookup from SubwordLookup

--- lattice_tagger/utils/lookup.py
+++ lattice_tagger/utils/lookup.py
@@ -38,14 +38,14 @@ def sentence_lookup(sent, eojeol_lookup):
     return nodes
 
 
-class Lookup:
+class EojeolLookup:
     def __call__(self, eojeol, offset=0):
         return self.lookup(eojeol, offset)
 
     def lookup(self, eojeol, offset):
         raise NotImplemented
 
-class LRLookup(Lookup):
+class LRLookup(EojeolLookup):
     def __init__(self, dictionary, prefer_exact_match=True):
         self.dictionary = dictionary
         self.prefer_exact_match = prefer_exact_match
@@ -53,6 +53,49 @@ class LRLookup(Lookup):
     def lookup(self, eojeol, offset=0):
         return lr_lookup(eojeol, self.dictionary, offset, self.prefer_exact_match)
 
+class SubwordLookup(EojeolLookup):
+    def __init__(self, dictionary, prefer_exact_match=True):
+        self.dictionary = dictionary
+        self.prefer_exact_match = prefer_exact_match
+
+    def lookup(self, eojeol, offset=0):
+        return subword_lookup(eojeol, self.dictionary, offset, self.prefer_exact_match)
+
+def subword_lookup(eojeol, dictionary, offset=0, prefer_exact_match=True):
+    """
+    >>> from lattice_tagger.utils import DemoMorphemeDictionary
+    >>> dictionary = DemoMorphemeDictionary()
+
+    >>> subword_lookup('아이오아이', dictionary)
+    $ [Word(아이오아이, 아이오아이/Noun, len=5, b=0, e=5)]
+
+    >>> subword_lookup('아이오아이', dictionary)
+    $ [Word(아이오아이, 아이오아이/Noun, len=5, b=2, e=7)]
+
+    >>> subword_lookup('아이오아이', dictionary, prefer_exact_match=False)
+    $ [Word(아이오아이, 아이오아이/Noun, len=5, b=0, e=5),
+       Word(아이, 아이/Noun, len=2, b=0, e=2),
+       Word(아이오, 아이오/Noun, len=3, b=0, e=3),
+       Word(아이오아이, 아이오아이/Noun, len=5, b=0, e=5),
+       Word(이, 이/Adjective, len=1, b=1, e=2),
+       Word(이, 이/Noun, len=1, b=1, e=2),
+       Word(이, 이/Josa, len=1, b=1, e=2),
+       Word(아이, 아이/Noun, len=2, b=3, e=5),
+       Word(이, 이/Adjective, len=1, b=4, e=5),
+       Word(이, 이/Noun, len=1, b=4, e=5),
+       Word(이, 이/Josa, len=1, b=4, e=5)]
+    """
+
+    words = dictionary.lookup(eojeol, offset)
+    if prefer_exact_match and words:
+        return words
+
+    n = len(eojeol)
+    for b in range(n):
+        for e in range(b+1, n+1):
+            sub = eojeol[b:e]
+            words += dictionary.lookup(sub, offset + b)
+    return words
 
 def lr_lookup(eojeol, dictionary, offset=0, prefer_exact_match=True):
     """
@@ -86,76 +129,7 @@ def lr_lookup(eojeol, dictionary, offset=0, prefer_exact_match=True):
         words += rset
     return words
 
-
-class SubwordLookup:
-    """
-    Assume that eojeol has space noise.
-    Check whether subword is enrolled in dictionary for all subword.
-
-    Usage
-    -----
-        >>> eojeol_lookup = SubwordLookup(morph_dict)
-        >>> eojeol_lookup('공연을했다', offset=7)
-
-        $ [(Word(공연, 공연/Noun, len=2), 7, 9),
-           (Word(을, 을/Josa, len=1), 9, 10),
-           (Word(했다, 하/Verb + 았다/Eomi, len=2), 10, 12)]
-    """
-
-    def __init__(self, dictionary):
-        self.dictionary = dictionary
-
-    def __call__(self, eojeol, offset=0):
-        return self.lookup(eojeol, offset)
-
-    def lookup(self, eojeol, offset=0):
-        words = []
-        n = len(eojeol)
-        for b in range(n):
-            for e in range(b+1, n+1):
-                sub = eojeol[b:e]
-                words += [
-                    (word, offset + b, offset + e) for word in self.dictionary.as_Word(sub)
-                    if not (word.tag0 == Eomi)
-                ]
-        return words
-
-
-class Node(namedtuple('Node', 'word morph0 morph1 tag0 tag1 b e len')):
-    """
-    Usage
-    -----
-        >>> node = Node('아이오아이', '아이오아이', None, 'Noun', None, 3, 8, 5)
-        >>> print(node)
-
-        $ Node(아이오아이, 아이오아이/Noun, 3, 8, len=5)
-
-        >>> node = Node('가고있는', '가고있', '는', 'Verb', 'Eomi', 0, 4, 4)
-        >>> print(node)
-
-        $ Node(가고있는, 가고있/Verb + 는/Eomi, 0, 4, len=4)
-
-        >>> node = Node('간', '가', 'ㄴ', 'Verb', 'Eomi', 5, 6, 1)
-        >>> print(node)
-
-        $ Node(간, 가/Verb + ㄴ/Eomi, 5, 6, len=1)
-
-    """
-
-    def __repr__(self):
-        return self.__str__()
-    def __str__(self):
-        if self.morph1:
-            args = (self.word, self.morph0, self.tag0,
-                    self.morph1, self.tag1, self.b, self.e, self.len)
-            return 'Node(%s, %s/%s + %s/%s, %d, %d, len=%d)' % args
-        args = (self.word, self.morph0, self.tag0, self.b, self.e, self.len)
-        return 'Node(%s, %s/%s, %d, %d, len=%d)' % args
-
-def word_posi_to_node(word, b, e):
-    return Node(word.word, word.morph0, word.morph1, word.tag0, word.tag1, b, e, word.len)
-
-def lookup_as_edge(sent, eojeol_lookup):
+def sentence_lookup_as_graph(sent, eojeol_lookup):
     """
     Arguments
     ---------
