1b3749687a62a791083a4bffa18a89bc825534b2
lovit <soy.lovit@gmail.com>
2019-03-25 06:55:51 +0900

    Beam search score functions as callable class

--- lattice_tagger/beam/score_funcs.py
+++ lattice_tagger/beam/score_funcs.py
@@ -1,23 +1,77 @@
 from ..tagset import *
 
-def trigram_beam_score(seq, word_k, **kargs):
-    base_score = seq.score
-    if len(seq.sequences) == 1:
-        word_i = None
-    else:
-        word_i = seq.sequences[-2]
-    word_j = seq.sequences[-1]
 
-    # TODO
-    # featurize
+class BeamScoreFunction:
+    def __call__(self, sequence, word_k):
+        return self.score(sequence, word_k)
 
-    # cumulate feature coefficient
-    score_increment = 0
+    def score(self, seq, word_k):
+        raise NotImplemented('Inherit and implement score function')
 
-    return score_increment
+class BeamScoreFunctions:
+    def __init__(self, *functions):
+        for func in functions:
+            if not isinstance(func, BeamScoreFunction):
+                raise ValueError('functions must be instance of BeamScoreFunction')
+        self.funcs = [func for func in functions]
 
-def penalty(seq, word_k, **kargs):
-    if word_k.tag0 == Unk:
-        return kargs.get('unknown_penalty', 0)
-    return 0.001
-    # return 0
+    def __call__(self, sequence, word_k):
+        return self.score(sequence, word_k)
+
+    def score(self, sequence, word_k):
+        score = 0
+        for func in self.funcs:
+            score += func(sequence, word_k)
+        return score
+
+class RegularizationScore(BeamScoreFunction):
+    def __init__(self, unknown_penalty=-0.1, known_preference=0.1):
+        self.unknown_penalty = unknown_penalty
+        self.known_preference = known_preference
+
+    def score(self, seq, word_k):
+        if word_k.tag0 == Unk:
+            return self.unknown_penalty
+        return self.known_preference * word_k.len
+
+class MorphemePreferenceScore(BeamScoreFunction):
+    def __init__(self, tag_to_morph=None):
+        if tag_to_morph is None:
+            tag_to_morph = {}
+        self.tag_to_morph = tag_to_morph
+
+    def score(self, seq, word_k):
+        score = self.tag_to_morph.get(word_k.tag0, {}).get(word_k.morph0, 0)
+        if word_k.tag1 is not None:
+            score += self.tag_to_morph.get(word_k.tag1, {}).get(word_k.morph1, 0)
+        return score
+
+class WordPreferenceScore(BeamScoreFunction):
+    def __init__(self, tag_to_word=None):
+        if tag_to_word is None:
+            tag_to_word = {}
+        self.tag_to_word = tag_to_word
+
+    def score(self, seq, word_k):
+        return self.tag_to_word.get(word_k.tag0, {}).get(word_k.word, 0)
+
+class FeatureScore(BeamScoreFunction):
+    def __init__(self, coefficient, encoder):
+        self.coefficient = coefficient
+        self.encoder = encoder
+        self.n_features = len(coefficient)
+
+    def score(self, seq, word_k):
+        if len(seq.sequences) == 1:
+            word_i = None
+        else:
+            word_i = seq.sequences[-2]
+        word_j = seq.sequences[-1]
+
+        # TODO
+        # featurize
+
+        # cumulate feature coefficient
+        score = 0
+
+        return score
